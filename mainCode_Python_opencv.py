78d8271302fc6986f74c99f6f7df8d2f2aaa742f
klcppp <azhongbo@gmail.com>
2020-10-05 06:36:16 +0800

    test

--- mainCode_Python_opencv.py
+++ mainCode_Python_opencv.py
@@ -46,24 +46,282 @@ MyCodeName = sys.argv[2]
 # runAllData(MyCodeTitle,MyCodeString,MyCodeName)
 
 
-# ### -------------------------------------------------------------------
-# MyCodeTitle  = "RyanCode Python opencv ( 範例 )"
-# MyCodeString = '''
-# ###  Python opencv 範例程式 ####
-# ### file: mainCode_Python_opencv
-# xxxxxxxxxxxxxxxxxxxxxxxxxxx
-# '''
-# runAllData(MyCodeTitle,MyCodeString,MyCodeName)
+### -------------------------------------------------------------------
+MyCodeTitle  = "RyanCode Python opencv ( Text 文件掃描-可調對比亮度 )"
+MyCodeString = '''
+###  Python opencv Text 文件掃描-可調對比亮度 ####
+### file: mainCode_Python_opencv
+import cv2
+import numpy as np
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 
 
-# ### -------------------------------------------------------------------
-# MyCodeTitle  = "RyanCode Python opencv ( 範例 )"
-# MyCodeString = '''
-# ###  Python opencv 範例程式 ####
-# ### file: mainCode_Python_opencv
-# xxxxxxxxxxxxxxxxxxxxxxxxxxx
-# '''
-# runAllData(MyCodeTitle,MyCodeString,MyCodeName)
+cap = cv2.VideoCapture(0)
+cap.set(10,150)
+
+###################################
+widthImg  = 540
+heightImg = 640
+#####################################
+
+windowName    = "dstImage"  # 視窗的名稱
+cv2.namedWindow(windowName,1)
+
+bright   = 255
+contrast = 127
+
+def empty(a): pass
+cv2.createTrackbar( 'Bright'   , windowName, bright   , 2*255, empty ) #Brightness value range -255 to 255
+cv2.createTrackbar( 'Contrast' , windowName, contrast , 2*127, empty ) #Contrast value range -127 to 127
+
+while True:
+    success, original = cap.read()
+    # original = cv2.imread("paper.jpg")
+    original = cv2.resize( original , (widthImg,heightImg))
+
+    img = original.copy()
+
+    bright   = cv2.getTrackbarPos( 'Bright'   , windowName )
+    contrast = cv2.getTrackbarPos( 'Contrast' , windowName ) 
+
+    #### 調整亮度 對比 ####################################################################
+    bright   = int(( bright -  0 ) * ( 255 + 255 ) / ( 510 - 0 ) -255 )
+    contrast = int(( contrast -0 ) * ( 127 + 127 ) / ( 254 - 0 ) -127 )
+    if bright != 0:
+        if bright > 0:
+            shadow = bright
+            highlight = 255
+        else:
+            shadow = 0
+            highlight = 255 + bright
+        alpha_b = (highlight - shadow)/255
+        gamma_b = shadow
+ 
+        effect = cv2.addWeighted(img, alpha_b, img, 0, gamma_b)
+    else:
+        effect = img.copy()
+ 
+    if contrast != 0:
+        f = float(131 * (contrast + 127)) / (127 * (131 - contrast))
+        alpha_c = f
+        gamma_c = 127*(1-f) 
+        effect = cv2.addWeighted(effect, alpha_c, effect, 0, gamma_c) 
+    #### END 調整亮度 對比 ####################################################################
+
+    ### 第一次圖片處理 #######################################################
+    imgGray  = cv2.cvtColor(effect,cv2.COLOR_BGR2GRAY)          ## 轉灰階 
+    imgBlur  = cv2.GaussianBlur(imgGray,(5,5),1)                ## 高斯模糊
+    imgCanny = cv2.Canny(imgBlur,200,200)                       ## 邊緣檢測
+    imgDial  = cv2.dilate(imgCanny,np.ones((5,5)),iterations=2) ## 增強邊緣檢測
+    imgThres = cv2.erode(imgDial  ,np.ones((5,5)),iterations=1) ## 減弱邊緣檢測
+    ### END 第一次圖片處理 ##################################################
+
+    imgContour = effect.copy()
+
+    ### 第二次圖片處理 #######################################################
+    biggest = np.array([])
+    maxArea = 0
+    contours,hierarchy = cv2.findContours(imgThres,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE) ## 找到邊角
+    for cnt in contours:
+        area = cv2.contourArea(cnt) ## 計算面積
+
+        if area>5000:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3) ## 畫出四邊
+            peri = cv2.arcLength(cnt,True)                        ## 取得 邊緣長度
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)         ## 取得 頂點數量
+            x,y,w,h = cv2.boundingRect(approx)                    ## 取得 頂點座標
+            cv2.putText(imgContour, str(int(area)) , (x+(w//2),y+(h//2)),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2) ## 畫出面積
+            if area >maxArea and len(approx) == 4:
+                biggest = approx
+                maxArea = area
+    cv2.drawContours(imgContour, biggest, -1, (0, 0, 255), 20)    ## 畫出四個角    
+    ### end 第二次圖片處理 #######################################################
+
+    ### 第三次圖片處理 #######################################################
+    if biggest.size !=0:
+        ################################
+        myPoints = biggest.reshape((4,2))
+        myPointsNew = np.zeros((4,1,2),np.int32)
+        add = myPoints.sum(1)
+        #print("add", add)
+        myPointsNew[0] = myPoints[np.argmin(add)]
+        myPointsNew[3] = myPoints[np.argmax(add)]
+        diff = np.diff(myPoints,axis=1)
+        myPointsNew[1]= myPoints[np.argmin(diff)]
+        myPointsNew[2] = myPoints[np.argmax(diff)]
+        biggest = myPointsNew
+        #print("NewPoints",myPointsNew)
+        ################################
+        pts1 = np.float32(biggest)
+        pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+        ################################
+        imgCropped = imgOutput[20:imgOutput.shape[0]-20,20:imgOutput.shape[1]-20]
+        imgWarped  = cv2.resize(imgCropped,(widthImg,heightImg))
+    ### end 第三次圖片處理 #######################################################
+
+    # img        = cv2.resize( img        , (widthImg,heightImg))
+    # imgCanny   = cv2.resize( imgCanny   , (widthImg,heightImg))
+    # imgThres   = cv2.resize( imgThres   , (widthImg,heightImg))
+    # imgContour = cv2.resize( imgContour , (widthImg,heightImg))
+    # imgWarped  = cv2.resize( imgWarped  , (widthImg,heightImg))
+
+    try:
+        cv2.imshow( windowName , stackImages( 0.6, ([img,imgCanny,imgThres,imgContour,imgWarped])     ) )
+    except:
+        cv2.imshow( windowName , stackImages( 0.6, ([img,imgCanny,imgThres,imgContour])     ) )
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+'''
+runAllData(MyCodeTitle,MyCodeString,MyCodeName)
+
+
+### -------------------------------------------------------------------
+MyCodeTitle  = "RyanCode Python opencv ( Text 文件掃描 )"
+MyCodeString = '''
+###  Python opencv 範例程式 ####
+### file: mainCode_Python_opencv
+import cv2
+import numpy as np
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+###################################
+widthImg  = 540
+heightImg = 640
+#####################################
+
+cap = cv2.VideoCapture(0)
+cap.set(10,150)
+
+windowName    = "dstImage"  # 視窗的名稱
+cv2.namedWindow(windowName,1)
+
+while True:
+    success, img = cap.read()
+    # img = cv2.imread("paper.jpg")
+
+    img = cv2.resize(img,(widthImg,heightImg))
+    imgContour = img.copy()
+
+    ### 第一次圖片處理 #######################################################
+    imgGray  = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)             ## 轉灰階 
+    imgBlur  = cv2.GaussianBlur(imgGray,(5,5),1)                ## 高斯模糊
+    imgCanny = cv2.Canny(imgBlur,200,200)                       ## 邊緣檢測
+    imgDial  = cv2.dilate(imgCanny,np.ones((5,5)),iterations=2) ## 增強邊緣檢測
+    imgThres = cv2.erode(imgDial  ,np.ones((5,5)),iterations=1) ## 減弱邊緣檢測
+    ### END 第一次圖片處理 ##################################################
+
+    ### 第二次圖片處理 #######################################################
+    biggest = np.array([])
+    maxArea = 0
+    contours,hierarchy = cv2.findContours(imgThres,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE) ## 找到邊角
+    for cnt in contours:
+        area = cv2.contourArea(cnt) ## 計算面積
+
+        if area>5000:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3) ## 畫出四邊
+            peri = cv2.arcLength(cnt,True)                        ## 取得 邊緣長度
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)         ## 取得 頂點數量
+            x,y,w,h = cv2.boundingRect(approx)                    ## 取得 頂點座標
+            cv2.putText(imgContour, str(int(area)) , (x+(w//2),y+(h//2)),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2) ## 畫出面積
+            if area >maxArea and len(approx) == 4:
+                biggest = approx
+                maxArea = area
+    cv2.drawContours(imgContour, biggest, -1, (0, 0, 255), 20)    ## 畫出四個角    
+    ### end 第二次圖片處理 #######################################################
+
+    ### 第三次圖片處理 #######################################################
+    if biggest.size !=0:
+        ################################
+        myPoints = biggest.reshape((4,2))
+        myPointsNew = np.zeros((4,1,2),np.int32)
+        add = myPoints.sum(1)
+        #print("add", add)
+        myPointsNew[0] = myPoints[np.argmin(add)]
+        myPointsNew[3] = myPoints[np.argmax(add)]
+        diff = np.diff(myPoints,axis=1)
+        myPointsNew[1]= myPoints[np.argmin(diff)]
+        myPointsNew[2] = myPoints[np.argmax(diff)]
+        biggest = myPointsNew
+        #print("NewPoints",myPointsNew)
+        ################################
+        pts1 = np.float32(biggest)
+        pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+        ################################
+        imgCropped = imgOutput[20:imgOutput.shape[0]-20,20:imgOutput.shape[1]-20]
+        imgWarped  = cv2.resize(imgCropped,(widthImg,heightImg))
+    ### end 第三次圖片處理 #######################################################
+
+    cv2.imshow( windowName , stackImages( 0.6, ([img,imgCanny,imgThres,imgContour,imgWarped])     ) )
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+'''
+runAllData(MyCodeTitle,MyCodeString,MyCodeName)
 
 
 ### -------------------------------------------------------------------
