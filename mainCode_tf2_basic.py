61846d98798ea17e1c09dee931595b3c1fc44c32
klcppp <azhongbo@gmail.com>
2020-11-06 07:49:58 +0800

    test

--- mainCode_tf2_basic.py
+++ mainCode_tf2_basic.py
@@ -272,7 +272,48 @@ plt.show()
 '''
 runAllData(MyCodeTitle,MyCodeString,MyCodeName)
 
+### -------------------------------------------------------------------
+MyCodeTitle  = "RyanCode tf2 Basic ( fashion mnist 圖像分類 )"
+MyCodeString = '''
+###  Tensorflow2 fashion mnist 圖像分類 ####
+### file: mainCode_tf2_basic
+# TensorFlow and tf.keras
+# https://www.tensorflow.org/tutorials/keras/classification
+# https://geektutu.com/post/tf2doc-ml-basic-image.html
 
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test accuracy:', test_acc)
+
+# 使用 predict函數進行預測
+predictions = model.predict(test_images)
+predictions[0]
+np.argmax(predictions[0]) # 9
+test_labels[0] # 9
+'''
+runAllData(MyCodeTitle,MyCodeString,MyCodeName)
 
 
 
