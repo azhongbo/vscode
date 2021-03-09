46fa9f7c35c280178e9f1b4103f6538b14e16332
klcppp <azhongbo@gmail.com>
2021-03-09 23:21:13 +0800

    test

--- mainCode_docker.py
+++ mainCode_docker.py
@@ -48,16 +48,30 @@ MyCodeString = '''
 
 ### 檔案: Dockerfile
 FROM ubuntu
+COPY startHttp /bin
+RUN chmod 755 /bin/startHttp
+
 RUN apt-get update
 RUN DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 RUN ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 RUN apt-get -y install net-tools nmap apache2 php7.4 curl systemd php7.4-sqlite3 apache2-dev php7.4-ldap php7.4-curl sqlite3 php7.4-curl php7.4-gd php7.4-pgsql php7.4-xml php7.4-bz2 php7.4-mbstring iputils-ping vim
+
 RUN echo "ServerName localhost:80" >> /etc/apache2/apache2.conf
+
 RUN echo "Asia/Taipei" > /etc/timezone
 RUN dpkg-reconfigure --frontend noninteractive tzdata
 RUN echo "Asia/Taipei" > /etc/timezone
+
+RUN apt-get -y install python3 python3-pip python3-tk python3-dev scrot
+RUN python3 -m pip install beautifulsoup4 flask jupyter jupyterlab keras lxml matplotlib numpy opencv-python pandas plotly pyautogui pygame pymssql python3-xlib requests selenium sklearn xlrd tensorflow==2.4.1
+RUN python3 -m pip install torch==1.8.0+cpu torchvision==0.9.0+cpu torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
+# RUN python3 -m pip install psycopg2
+
 EXPOSE 80
-CMD ["apachectl", "-D", "FOREGROUND"]
+CMD ["startHttp"]
+
+
+
 
 
 ### 檔案: startHttp
