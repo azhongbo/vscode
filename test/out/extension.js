a0dd5811fc580f0cf321c1d3921cecd1e429b949
azhongbo <32501317+azhongbo@users.noreply.github.com>
2026-03-01 23:26:42 +0800

    Add files via upload

--- test/out/extension.js
+++ test/out/extension.js
@@ -1,1 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode = require("vscode");
 
+const CursorMonitor = require("./cursorMonitor");
+
+
+let monitor;
+
+function activate(context) {
+    console.log('擴充功能已啟動');
+
+    // 建立實例並啟動 (設定 3000 毫秒)
+    monitor = new CursorMonitor(3000);
+    monitor.start(context);
+}
+
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() {
+    if (monitor) {
+        monitor.stop();
+    }
+}
+exports.deactivate = deactivate;
